85630e10b276385502f61152435b964dbb2432a6
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-12 16:31:16 +0100

    feat(repository): Adding IReferenceRepository and ReferenceQueries

---
+++ api/src/main/java/io/ski/api/persistance/repository/reference/ReferenceQueries.java
@@ -0,0 +1,5 @@
+package io.ski.api.persistance.repository.reference;
+
+public class ReferenceQueries {
+    public static final String FIND_BY_NAME = "Select r FROM Refs r WHERE r.name = :name";
+}
