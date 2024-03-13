82c8f5d2bfdc097023799d0405127aaae20d4594
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-13 10:19:47 +0100

    feat(DTO): Adding CartDTO

--- api/src/main/java/io/ski/api/business/dto/CartDto.java
+++ api/src/main/java/io/ski/api/business/dto/CartDto.java
@@ -2,4 +2,23 @@ package io.ski.api.business.dto;
 
 public class CartDto {
 
+    private long id;
+
+    /**
+     * Get the cart's id.
+     *
+     * @return the cart's id.
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Set the cart's id.
+     *
+     * @param id the cart's id to set.
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
 }
