263bc2db6ea2bbbf8d3ae022eb97eee13189e661
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-13 17:59:29 +0100

    feat(Service): Adding CartService

--- api/src/main/java/io/ski/api/business/service/cart/ICartService.java
+++ api/src/main/java/io/ski/api/business/service/cart/ICartService.java
@@ -1,4 +1,35 @@
 package io.ski.api.business.service.cart;
 
+import io.ski.api.business.dto.CartDto;
+
 public interface ICartService {
+
+    /**
+     * Saves a cart in the database.
+     *
+     * @param cart the user to save
+     */
+    void createCart(final CartDto cart);
+
+    /**
+     * Returns the cart with the given id.
+     *
+     * @param id the id of the cart
+     * @return the cart with the given id
+     */
+    CartDto getByUserCart(final long id);
+
+    /**
+     * Modifies the cart.
+     *
+     * @param cart the cart to modify
+     */
+    void modifyCart(final CartDto cart);
+
+    /**
+     * Deletes the cart with the given id.
+     *
+     * @param id the id of the cart
+     */
+    void deleteCart(final long id);
 }
