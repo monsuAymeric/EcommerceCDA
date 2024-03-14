b4f7be95505470f5a7bf7b068c4b5a82b7fd4555
Sarah Katz <sarakatz59@gmail.com>
2024-03-14 09:40:14 +0100

    fix(repository): Added missing annotation

--- api/src/main/java/io/ski/api/persistance/repository/product/IProductRepository.java
+++ api/src/main/java/io/ski/api/persistance/repository/product/IProductRepository.java
@@ -3,7 +3,9 @@ package io.ski.api.persistance.repository.product;
 import io.ski.api.persistance.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IProductRepository extends JpaRepository<Product, Long> {
     @Query(ProductQueries.FIND_BY_REF)
     Product findByRef(final long id_ref);
--- api/src/main/java/io/ski/api/persistance/repository/reference/IReferenceRepository.java
+++ api/src/main/java/io/ski/api/persistance/repository/reference/IReferenceRepository.java
@@ -3,9 +3,11 @@ package io.ski.api.persistance.repository.reference;
 import io.ski.api.persistance.entity.Reference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IReferenceRepository extends JpaRepository<Reference, Long> {
 
     @Query(ReferenceQueries.FIND_BY_NAME)
