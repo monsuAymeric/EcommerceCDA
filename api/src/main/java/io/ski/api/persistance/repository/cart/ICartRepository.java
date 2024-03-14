8aec8a61da4584e4c21e4a59922afed6e984467a
Sarah Katz <sarakatz59@gmail.com>
2024-03-14 09:35:42 +0100

    fix: Wrong entity used in repo

--- api/src/main/java/io/ski/api/persistance/repository/cart/ICartRepository.java
+++ api/src/main/java/io/ski/api/persistance/repository/cart/ICartRepository.java
@@ -1,11 +1,10 @@
 package io.ski.api.persistance.repository.cart;
 
 import io.ski.api.persistance.entity.Cart;
-import io.ski.api.persistance.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ICartRepository extends JpaRepository<Product, Long> {
+public interface ICartRepository extends JpaRepository<Cart, Long> {
     @Query(CartQueries.FIND_BY_USER)
     Cart findByUser(final long id);
 }
