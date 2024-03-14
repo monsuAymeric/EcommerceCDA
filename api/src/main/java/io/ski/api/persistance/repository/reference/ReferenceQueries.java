e1da1acef744e4939028e37884521792b03fa0d6
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-14 20:23:17 +0100

    fix: Added private constructor to query utility classes

--- api/src/main/java/io/ski/api/persistance/repository/reference/ReferenceQueries.java
+++ api/src/main/java/io/ski/api/persistance/repository/reference/ReferenceQueries.java
@@ -1,5 +1,10 @@
 package io.ski.api.persistance.repository.reference;
 
 public class ReferenceQueries {
+
+    private ReferenceQueries() {
+        throw new IllegalStateException("Utility class not meant for instantiation!");
+    }
+
     public static final String FIND_BY_NAME = "Select r FROM Reference r WHERE r.name = :name";
 }
--- api/src/main/java/io/ski/api/persistance/repository/user/UserQueries.java
+++ api/src/main/java/io/ski/api/persistance/repository/user/UserQueries.java
@@ -1,5 +1,10 @@
 package io.ski.api.persistance.repository.user;
 
 public class UserQueries {
+
+    private UserQueries() {
+        throw new IllegalStateException("Utility class not meant for instantiation!");
+    }
+
     public static final String FIND_BY_USERNAME = "SELECT u FROM User u WHERE u.username = :username";
 }
